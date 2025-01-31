6bfc8debf6a6268ba1d17b488c1cd8ee5978cc36
Sean Parsons <sparsonslab@gmail.com>
2025-01-31 14:36:20 +0000

    app folder companion

--- app/src/main/java/com/scepticalphysiologist/dmaple/MainActivity.kt
+++ app/src/main/java/com/scepticalphysiologist/dmaple/MainActivity.kt
@@ -2,12 +2,21 @@ package com.scepticalphysiologist.dmaple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+
+    companion object {
+        /** The system-created storage directory specific for this app. */
+        var storageDirectory: File? = null
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        storageDirectory = applicationContext.getExternalFilesDir(null)
     }
 
 }
