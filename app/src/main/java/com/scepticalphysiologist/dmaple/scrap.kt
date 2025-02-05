22e1e9dbd9dcea3805726fed3487f3de8bcc78cb
Sean Parsons <sparsonslab@gmail.com>
2025-02-05 18:24:29 +0000

    tiff images - working example

--- app/src/main/java/com/scepticalphysiologist/dmaple/scrap.kt
+++ app/src/main/java/com/scepticalphysiologist/dmaple/scrap.kt
@@ -1,56 +1,45 @@
 package com.scepticalphysiologist.dmaple
 
-import android.os.SystemClock.sleep
-import com.scepticalphysiologist.dmaple.io.FileBackedBuffer
+
+import com.scepticalphysiologist.dmaple.io.createByteChannelTiff
+import mil.nga.tiff.TiffWriter
 import java.io.File
-import java.io.FileOutputStream
-import java.io.RandomAccessFile
-import java.nio.channels.FileChannel
-import java.util.concurrent.TimeUnit
-import kotlin.random.Random
+import java.nio.ByteBuffer
+import kotlin.math.PI
+import kotlin.math.sin
+
 
 
 fun main() {
 
+    val w = 100
+    val h = 128
+    val waveLength = 52f
+    val bytes = ByteArray(w * h)
+    for(i in bytes.indices)
+        bytes[i] = (127 * sin(i.toFloat() * 2 * PI / waveLength)).toInt().toByte()
+    //println(bytes.slice(0..100).toList())
 
-    val dirName = "/Users/senparsons/Documents/programming/personal/dmaple_android"
-    val fileName = "large_buffer.dat"
-
-    // Check for file.
-    val file = File(dirName, fileName)
-    if(!file.exists()) file.createNewFile()
-
-    // Make sure file is large enough.
-    val targetSize = 10_000_000L
-    val fileSize = file.length()
-    if(fileSize < targetSize) {
-        /*
-        val strm = FileOutputStream(file, true)
-        val n = targetSize - fileSize
-        for(i in 1..n) strm.write(0)
-        strm.close()
-         */
-
-        val strm = RandomAccessFile(file, "rw")
-        strm.setLength(targetSize)
-        strm.close()
-    }
-
-    //
-    val strm = RandomAccessFile(file, "rw")
-    val buffer = strm.channel.map(FileChannel.MapMode.READ_WRITE, 0, 11_000)
+    val img = createByteChannelTiff(ByteBuffer.wrap(bytes), w, h, 1)
+    var path = "/Users/senparsons/Documents/programming/personal/dmaple_android/example.tiff"
+    TiffWriter.writeTiff(File(path), img)
 
 
-    //
-    buffer.putDouble(10_450, 1.2345678)
-    println(buffer.getDouble(10_450))
-    println(buffer.getDouble(10_449))
-    println(buffer.getDouble(10_400))
+    val buff = ByteBuffer.allocate(w * h * 3)
+    var k: Int
+    for(j in 20..60)
+        for(i in 20..60) {
+            k = 3 * (j * w + i)
+            buff.put(k, -128)
+            buff.put(k + 1, 127)
+            buff.put(k + 2, 0)
+        }
+
+    val img2 = createByteChannelTiff(buff, w, h, 3)
+    path = "/Users/senparsons/Documents/programming/personal/dmaple_android/example2.tiff"
+    TiffWriter.writeTiff(File(path), img2)
 
 
-    strm.close()
-
-    //TimeUnit.SECONDS.sleep(30)
 
 }
 
