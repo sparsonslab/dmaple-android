6b42e779cef316df02665bfe2829ac9bc28c4bd6
Sean Parsons <sparsonslab@gmail.com>
2025-03-07 10:35:55 +0000

    comment

--- app/src/main/java/com/scepticalphysiologist/dmaple/ui/Recorder.kt
+++ app/src/main/java/com/scepticalphysiologist/dmaple/ui/Recorder.kt
@@ -132,12 +132,14 @@ class Recorder : DMapLEPage<RecorderBinding>(RecorderBinding::inflate) {
         // Set the camera and map views.
         binding.cameraAndRoi.freezeField(field)
         if(show) {
+            // The camera field in shown over the top corner of the map, and editing of ROIs is blocked.
             binding.toRecordsButton.visibility = View.INVISIBLE
             binding.cameraAndRoi.allowEditing(false)
             val extent = Point.ofViewExtent(binding.root) * 0.5f
             binding.cameraAndRoi.resize(extent.x.toInt(), extent.y.toInt())
             showMapAndCreator(model.getCurrentlyShownMap())
         } else {
+            // The camera field takes up the whole screen and ROIs can be edited.
             binding.toRecordsButton.visibility = View.VISIBLE
             binding.cameraAndRoi.allowEditing(true)
             binding.cameraAndRoi.fullSize()
