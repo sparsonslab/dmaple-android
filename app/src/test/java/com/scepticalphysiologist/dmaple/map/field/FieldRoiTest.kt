1cf5f50e3b4756190a6675bd7c78bb6ed5de5b14
Sean Parsons <sparsonslab@gmail.com>
2025-03-22 18:03:48 +0000

    field roi unit test

--- app/src/test/java/com/scepticalphysiologist/dmaple/map/field/FieldRoiTest.kt
+++ app/src/test/java/com/scepticalphysiologist/dmaple/map/field/FieldRoiTest.kt
@@ -12,14 +12,13 @@ import java.util.stream.Stream
 
 class FieldRoiTest {
 
-
     private companion object {
 
         val frameSize = Point(100f, 100f)
 
         @JvmStatic
-        fun TransformCases() = Stream.of(
-            Arguments.of(
+        fun SetsOfEquivalentRoisInDifferentFrames() = Stream.of(
+            Arguments.of(listOf(
                 FieldRoi(
                     frame=Frame(size = frameSize, orientation = 0),
                     c0 = Point(10f, 10f), c1 = Point(60f, 60f),
@@ -30,38 +29,53 @@ class FieldRoiTest {
                     c0 = Point(10f, 40f), c1 = Point(60f, 90f),
                     seedingEdge = Edge.TOP
                 ),
-            ),
-            Arguments.of(
                 FieldRoi(
-                    frame=Frame(size = frameSize, orientation = 0),
-                    c0 = Point(10f, 10f), c1 = Point(60f, 60f),
+                    frame=Frame(size = frameSize, orientation = -90),
+                    c0 = Point(40f, 10f), c1 = Point(90f, 60f),
                     seedingEdge = Edge.BOTTOM
                 ),
                 FieldRoi(
                     frame=Frame(size = frameSize, orientation = -180),
                     c0 = Point(40f, 40f), c1 = Point(90f, 90f),
+                    seedingEdge = Edge.LEFT
+                ),
+            )),
+            Arguments.of(listOf(
+                FieldRoi(
+                    frame=Frame(size = frameSize, orientation = 0),
+                    c0 = Point(30f, 30f), c1 = Point(90f, 40f),
+                    seedingEdge = Edge.BOTTOM
+                ),
+                FieldRoi(
+                    frame=Frame(size = frameSize, orientation = -180),
+                    c0 = Point(10f, 60f), c1 = Point(70f, 70f),
                     seedingEdge = Edge.TOP
                 ),
-            ),
+                FieldRoi(
+                    frame=Frame(size = frameSize, orientation = -270),
+                    c0 = Point(30f, 10f), c1 = Point(40f, 70f),
+                    seedingEdge = Edge.RIGHT
+                ),
+            )),
         )
 
     }
 
-    @ParameterizedTest(name = "{index} ==> transform {0} to {1}")
-    @MethodSource("TransformCases")
-    fun `correct transform`(actualRoi: FieldRoi, expectedRoi: FieldRoi) {
-        // Given: An ROI
-        // [actualRoi]
+    @ParameterizedTest(name = "{index} ==> ")
+    @MethodSource("SetsOfEquivalentRoisInDifferentFrames")
+    fun `transforms are correct`(rois: List<FieldRoi>) {
+        // Given: A list of ROIs that are equivalent but in different frames.
+        for(i in 1 until rois.size) {
 
-        // When: It is transformed to another frame.
-        actualRoi.changeFrame(expectedRoi.frame)
+            // When: Each ROI is transformed to the frame of the first ROI.
+            rois[i].changeFrame(rois[0].frame)
 
-        // Then: The transformed ROI is as expected.
-        assertEquals(expectedRoi.left, actualRoi.left, 1e-4f)
-        assertEquals(expectedRoi.right, actualRoi.right, 1e-4f)
-        assertEquals(expectedRoi.top, actualRoi.top, 1e-4f)
-        assertEquals(expectedRoi.bottom, actualRoi.bottom, 1e-4f)
-        assertEquals(expectedRoi.seedingEdge, actualRoi.seedingEdge)
+            // Then: The transformed ROI is equal to the first ROI.
+            assertEquals(rois[0].left,        rois[i].left, 1e-4f)
+            assertEquals(rois[0].right,       rois[i].right, 1e-4f)
+            assertEquals(rois[0].top,         rois[i].top, 1e-4f)
+            assertEquals(rois[0].bottom,      rois[i].bottom, 1e-4f)
+            assertEquals(rois[0].seedingEdge, rois[i].seedingEdge)
+        }
     }
-
 }
