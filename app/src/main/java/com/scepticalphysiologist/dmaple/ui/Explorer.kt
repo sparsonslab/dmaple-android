9bacd18f033195ec9d5c941232051a2dc7c423fa
Sean Parsons <sparsonslab@gmail.com>
2025-03-23 19:20:19 +0000

    build errors with duplicate classes

--- app/src/main/java/com/scepticalphysiologist/dmaple/ui/Explorer.kt
+++ app/src/main/java/com/scepticalphysiologist/dmaple/ui/Explorer.kt
@@ -1,15 +1,34 @@
 package com.scepticalphysiologist.dmaple.ui
 
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.scepticalphysiologist.dmaple.databinding.ExplorerBinding
-import com.scepticalphysiologist.dmaple.map.record.MappingRecordAdaptor
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import com.scepticalphysiologist.dmaple.map.record.MappingRecord
 
 
-class Explorer: DMapLEPage<ExplorerBinding>(ExplorerBinding::inflate) {
+class Explorer: Fragment() {
 
-    override fun createUI() {
-        // Set recycler view properties, layout and adaptor.
-        binding.mappingRecords.setAdapter(MappingRecordAdaptor(this))
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = ComposeView(requireActivity())
+        view.setContent {
+            LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 200.dp)) {
+                items(200) { i ->
+                    Text(text = "This is item $i")
+                }
+            }
+        }
+        return view
     }
 
 }
