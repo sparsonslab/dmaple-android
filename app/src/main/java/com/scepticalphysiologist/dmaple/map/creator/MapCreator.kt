0b6a43242219498440bac3a4c8c69bf7340ed6af
Sean Parsons <sparsonslab@gmail.com>
2025-03-27 19:14:43 +0000

    split out maptype enum own module

--- app/src/main/java/com/scepticalphysiologist/dmaple/map/creator/MapCreator.kt
+++ app/src/main/java/com/scepticalphysiologist/dmaple/map/creator/MapCreator.kt
@@ -17,20 +17,6 @@ import java.lang.IndexOutOfBoundsException
 import java.nio.ByteBuffer
 import kotlin.math.ceil
 
-/** The types of maps that be created. */
-enum class MapType (
-    /** The title of the map. */
-    val title: String,
-    /** The number of maps created for the type. */
-    val nMaps: Int,
-    /** The number of bytes required per space-time sample/pixel per map. */
-    val bytesPerSample: Int
-){
-    DIAMETER(title = "diameter", nMaps = 1, bytesPerSample = 2),
-    RADIUS(title = "radius", nMaps = 2, bytesPerSample = 2),
-    SPINE(title = "spine profile", nMaps = 1, bytesPerSample = 3);
-}
-
 /** Handles the creation of spatio-temporal maps for a single ROI. */
 class MapCreator(val roi: FieldRoi) {
 
---
+++ app/src/main/java/com/scepticalphysiologist/dmaple/map/creator/MapType.kt
@@ -0,0 +1,15 @@
+package com.scepticalphysiologist.dmaple.map.creator
+
+/** The types of maps that be created. */
+enum class MapType (
+    /** The title of the map. */
+    val title: String,
+    /** The number of maps created for the type. */
+    val nMaps: Int,
+    /** The number of bytes required per space-time sample/pixel per map. */
+    val bytesPerSample: Int
+){
+    DIAMETER(title = "diameter", nMaps = 1, bytesPerSample = 2),
+    RADIUS(title = "radius", nMaps = 2, bytesPerSample = 2),
+    SPINE(title = "spine profile", nMaps = 1, bytesPerSample = 3);
+}
