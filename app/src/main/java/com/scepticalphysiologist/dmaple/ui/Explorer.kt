073c85b47e801e6f2d42c912c2a5943fa1f5df0a
Sean Parsons <sparsonslab@gmail.com>
2025-04-07 13:16:56 +0100

    unused import

--- app/src/main/java/com/scepticalphysiologist/dmaple/ui/Explorer.kt
+++ app/src/main/java/com/scepticalphysiologist/dmaple/ui/Explorer.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
