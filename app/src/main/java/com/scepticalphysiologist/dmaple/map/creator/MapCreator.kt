7601f7c79706420f15568e626b46136a4a5ae902
Sean Parsons <sparsonslab@gmail.com>
2025-04-07 13:52:08 +0100

    tiff functions from mapcreator to own module

--- app/src/main/java/com/scepticalphysiologist/dmaple/map/creator/MapCreator.kt
+++ app/src/main/java/com/scepticalphysiologist/dmaple/map/creator/MapCreator.kt
@@ -10,7 +10,6 @@ import com.scepticalphysiologist.dmaple.map.buffer.RGBMap
 import com.scepticalphysiologist.dmaple.map.buffer.ShortMap
 import com.scepticalphysiologist.dmaple.map.field.FieldRoi
 import com.scepticalphysiologist.dmaple.map.field.FieldRuler
-import mil.nga.tiff.FieldTagType
 import mil.nga.tiff.FileDirectory
 import mil.nga.tiff.TiffReader
 import java.io.File
@@ -20,7 +19,6 @@ import java.lang.IndexOutOfBoundsException
 import java.nio.ByteBuffer
 import java.util.concurrent.ForkJoinPool
 import kotlin.math.abs
-import kotlin.math.ceil
 
 /** Handles the creation of spatio-temporal maps for a single ROI. */
 class MapCreator(val roi: FieldRoi, val params: FieldParams) {
@@ -278,68 +276,4 @@ class MapCreator(val roi: FieldRoi, val params: FieldParams) {
         }
         return true
     }
-
-}
-
-// -------------------------------------------------------------------------------------------------
-// I/O
-// -------------------------------------------------------------------------------------------------
-
-/** Find a tiff directory with a matching identifier. */
-fun findTiff(tiffs: List<FileDirectory>, identifier: String): FileDirectory? {
-    return tiffs.filter{it.getStringEntryValue(FieldTagType.ImageUniqueID) == identifier}.firstOrNull()
-}
-
-/** Set the x and y resolutions of a tiff directory according to the ImageJ format.
- *
- * @param tiff The TIFF directory.
- * @param xr The x resolution (pixels/unit).
- * @param yr The y resolution (pixels/unit).
- * */
-fun setResolution(tiff: FileDirectory, xr: Pair<Float, String>, yr: Pair<Float, String>) {
-    tiff.setRationalEntryValue(FieldTagType.XResolution, floatToRational(xr.first))
-    tiff.setRationalEntryValue(FieldTagType.YResolution, floatToRational(yr.first))
-    val imagejDescription = listOf(
-        "ImageJ=1.53k",
-        "unit=${xr.second}", "yunit=${yr.second}", "zunit=-",
-        "vunit=${xr.second}", "cf=0", "c0=0", "c1=${1f / xr.first}"
-    ).joinToString("\n")
-    tiff.setStringEntryValue(FieldTagType.ImageDescription, imagejDescription)
-}
-
-/** Get the x and y resolutions of a tiff directory. */
-fun getResolution(tiff: FileDirectory): Pair<Pair<Float, String>, Pair<Float, String>> {
-    try {
-        val xr = rationalToFloat(tiff.getLongListEntryValue(FieldTagType.XResolution))
-        val yr = rationalToFloat(tiff.getLongListEntryValue(FieldTagType.YResolution))
-        var xu = ""
-        var yu = ""
-        val description = tiff.getStringEntryValue(FieldTagType.ImageDescription)
-        for(line in description.split("\n")){
-            val entry = line.split("=")
-            if(entry.size != 2) continue
-            when(entry[0]) {
-                "unit" -> xu = entry[1]
-                "yunit" -> yu = entry[1]
-            }
-        }
-        return Pair(Pair(xr, xu), Pair(yr, yu))
-    } catch(_: java.lang.ClassCastException) {}
-    return Pair(Pair(1f, ""), Pair(1f, ""))
-}
-
-fun rangeSize(range: Int, step: Int): Int {
-    return ceil(range.toFloat() / step.toFloat()).toInt()
-}
-
-/** Convert a float to a pair of numerator and denominator long integers. */
-fun floatToRational(value: Float, denom: Long = 100_000): List<Long> {
-    val num = (value * denom).toLong()
-    return listOf(num, denom)
-}
-
-/** Convert a pair of numerator and denominator long integers to a float. */
-fun rationalToFloat(ratio: List<Long>): Float {
-    if(ratio.size < 2) return 1f
-    return ratio[0].toFloat() / ratio[1].toFloat()
 }
---
+++ app/src/main/java/com/scepticalphysiologist/dmaple/map/creator/tiff.kt
@@ -0,0 +1,65 @@
+package com.scepticalphysiologist.dmaple.map.creator
+
+import mil.nga.tiff.FieldTagType
+import mil.nga.tiff.FileDirectory
+import kotlin.math.ceil
+
+
+/** Find a tiff directory with a matching identifier. */
+fun findTiff(tiffs: List<FileDirectory>, identifier: String): FileDirectory? {
+    return tiffs.filter{it.getStringEntryValue(FieldTagType.ImageUniqueID) == identifier}.firstOrNull()
+}
+
+/** Set the x and y resolutions of a tiff directory according to the ImageJ format.
+ *
+ * @param tiff The TIFF directory.
+ * @param xr The x resolution (pixels/unit).
+ * @param yr The y resolution (pixels/unit).
+ * */
+fun setResolution(tiff: FileDirectory, xr: Pair<Float, String>, yr: Pair<Float, String>) {
+    tiff.setRationalEntryValue(FieldTagType.XResolution, floatToRational(xr.first))
+    tiff.setRationalEntryValue(FieldTagType.YResolution, floatToRational(yr.first))
+    val imagejDescription = listOf(
+        "ImageJ=1.53k",
+        "unit=${xr.second}", "yunit=${yr.second}", "zunit=-",
+        "vunit=${xr.second}", "cf=0", "c0=0", "c1=${1f / xr.first}"
+    ).joinToString("\n")
+    tiff.setStringEntryValue(FieldTagType.ImageDescription, imagejDescription)
+}
+
+/** Get the x and y resolutions of a tiff directory. */
+fun getResolution(tiff: FileDirectory): Pair<Pair<Float, String>, Pair<Float, String>> {
+    try {
+        val xr = rationalToFloat(tiff.getLongListEntryValue(FieldTagType.XResolution))
+        val yr = rationalToFloat(tiff.getLongListEntryValue(FieldTagType.YResolution))
+        var xu = ""
+        var yu = ""
+        val description = tiff.getStringEntryValue(FieldTagType.ImageDescription)
+        for(line in description.split("\n")){
+            val entry = line.split("=")
+            if(entry.size != 2) continue
+            when(entry[0]) {
+                "unit" -> xu = entry[1]
+                "yunit" -> yu = entry[1]
+            }
+        }
+        return Pair(Pair(xr, xu), Pair(yr, yu))
+    } catch(_: java.lang.ClassCastException) {}
+    return Pair(Pair(1f, ""), Pair(1f, ""))
+}
+
+fun rangeSize(range: Int, step: Int): Int {
+    return ceil(range.toFloat() / step.toFloat()).toInt()
+}
+
+/** Convert a float to a pair of numerator and denominator long integers. */
+fun floatToRational(value: Float, denom: Long = 100_000): List<Long> {
+    val num = (value * denom).toLong()
+    return listOf(num, denom)
+}
+
+/** Convert a pair of numerator and denominator long integers to a float. */
+fun rationalToFloat(ratio: List<Long>): Float {
+    if(ratio.size < 2) return 1f
+    return ratio[0].toFloat() / ratio[1].toFloat()
+}
