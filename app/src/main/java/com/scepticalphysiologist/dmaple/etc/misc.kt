b447cb301c3b0962a379308c5f4f8a4a4b4f9608
Sean Parsons <sparsonslab@gmail.com>
2025-04-09 15:49:39 +0100

    move strftime

--- app/src/main/java/com/scepticalphysiologist/dmaple/etc/misc.kt
+++ app/src/main/java/com/scepticalphysiologist/dmaple/etc/misc.kt
@@ -1,6 +1,5 @@
 package com.scepticalphysiologist.dmaple.etc
 
-import java.time.Duration
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -12,19 +11,6 @@ import kotlin.random.Random
 fun randomAlphaString(n: Int): String {
     val alphas = ('a'..'z').map{it} + ('A'..'Z').map{it}
     return (0 until n).map{alphas[Random.nextInt(alphas.size)]}.joinToString("")
-}
-
-
-/** Time to formatted string, much like Python's strftime. */
-fun strftime(time: Instant, format: String): String {
-    val formatter = DateTimeFormatter.ofPattern(format).withZone(ZoneId.systemDefault())
-    return formatter.format(time)
-}
-
-fun strfduration(duration: Duration): String {
-    return String.format(
-        "%02d:%02d:%02d", duration.toHours(), duration.toMinutesPart(), duration.toSecondsPart()
-    )
 }
 
 fun strptime(time: String, format: String): Instant? {
--- app/src/main/java/com/scepticalphysiologist/dmaple/map/record/RecordMetadata.kt
+++ app/src/main/java/com/scepticalphysiologist/dmaple/map/record/RecordMetadata.kt
@@ -1,9 +1,9 @@
 package com.scepticalphysiologist.dmaple.map.record
 
-import com.scepticalphysiologist.dmaple.etc.strfduration
-import com.scepticalphysiologist.dmaple.etc.strftime
 import java.time.Duration
 import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 /** Some metadata about a recording. */
 class RecordMetadata(
@@ -11,5 +11,16 @@ class RecordMetadata(
     endTime: Instant
 ){
     var startDateTime: String = strftime(startTime, "dd/MM/YYYY, HH:mm")
-    var duration: String = strfduration(Duration.between(startTime, endTime))
+    var duration: String = strfduration(Duration.between(startTime, endTime), "%02d:%02d:%02d")
+}
+
+/** Time to formatted string, much like Python's strftime. */
+fun strftime(time: Instant, format: String): String {
+    val formatter = DateTimeFormatter.ofPattern(format).withZone(ZoneId.systemDefault())
+    return formatter.format(time)
+}
+
+/** Duration to formatted string. */
+fun strfduration(duration: Duration, format: String): String {
+    return String.format(format , duration.toHours(), duration.toMinutesPart(), duration.toSecondsPart())
 }
